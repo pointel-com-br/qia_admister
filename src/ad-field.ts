c63518118e7cec14a5f2594c4dd3ab3da9078998
Éverton M. Vieira <emuvi@outlook.com.br>
2023-01-05 19:37:00 -0300

    never has mutations when is fixed

--- src/ad-field.ts
+++ src/ad-field.ts
@@ -167,6 +167,9 @@ export class AdField {
   }
 
   public hasMutations(): boolean {
+    if (this.isFixed()) {
+      return false;
+    }
     let early = this._value;
     if (early === "") {
       early = null;
