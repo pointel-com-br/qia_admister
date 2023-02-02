ec954f1125b2af9fe32d13bff1c99be1dbe2d70c
Éverton M. Vieira <emuvi@outlook.com.br>
2023-02-02 20:32:17 -0300

    styles the table with no line wraps

--- src/ad-reg-table.ts
+++ src/ad-reg-table.ts
@@ -7,6 +7,7 @@ export class AdRegTable extends QinTable {
   public constructor(register: AdRegister) {
     super({ singleSelection: true });
     this._reg = register;
+    this.styleAsWhiteSpaceNoWrap();
     this.addOnLineMainAct((row, values: string[]) => {
       this._reg.tryTurnNoticeRow(row, values);
     });
